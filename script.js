4e9d08151adba9be3b4b3453d203463fcd5cd08c
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2019-12-02 11:08:55 +0100

    Exercice 2 JS

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 /*Exercice 1*/
 let numberOfSeasons = 6;
 let numberOfEpisodes = 12;
-console.log(numberOfSeasons, numberOfEpisodes);
+//console.log(numberOfSeasons, numberOfEpisodes);
+
+/*Exercice 2 */
+
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalShowTime = episodeTime + commercialTime * numberOfSeasons + numberOfEpisodes;
+
+console.log(totalShowTime);
